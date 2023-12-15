6f2993f8eb4f9d4ecf9a1cc5e4e4553d91fd0a65
Felix Schweizer <felix.schweizer@icf.ch>
2023-12-15 09:56:31 +0100

    fetch fake Data from API

--- pages/products/index.vue
+++ pages/products/index.vue
@@ -1,6 +1,16 @@
 <template>
     <div>
-<p>products</p>
+<div class="grid grid-cols-4 gap-5">
+    <div v-for="product in products" :key="product.id">
+        <nuxt-link :to="`/products/${product.id}`">
+            <div class="bg-white shadow-sm rounded-md p-4">
+                <img :src="product.image" :alt="product.title" class="w-full h-48 object-cover rounded-md">
+                <h3 class="font-bold text-lg">{{product.title}}</h3>
+                <p class="text-gray-500">{{product.price}}</p>
+            </div>
+        </nuxt-link>
+    </div>
+</div>
     </div>
 </template>
 
@@ -9,8 +19,8 @@ definePageMeta({
     layout: "products"
 })
 
+//fetch products
+const { data: products } = await useFetch("https://fakestoreapi.com/products")
 </script>
 
-<style  scoped>
-
-</style>
+<style  scoped></style>
